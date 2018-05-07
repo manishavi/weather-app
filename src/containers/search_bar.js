bd3ca2d16c4f3b956b07107adba21365e9138138
manishavi <manishalal972@gmail.com>
2018-05-07 18:02:06 -0500

    hooking up fetchWeather with SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   state = {
     term: '',
   };
@@ -11,6 +14,8 @@ export default class SearchBar extends Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render () {
@@ -29,3 +34,9 @@ export default class SearchBar extends Component {
     );  
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
