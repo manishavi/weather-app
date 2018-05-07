87862a41a5fc1acf1d7451de45d7d653651f10bd
manishavi <manishalal972@gmail.com>
2018-05-07 18:01:08 -0500

    updating typo in ROOT_URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '8139747d537ab0edf996b42c59cf1488';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
