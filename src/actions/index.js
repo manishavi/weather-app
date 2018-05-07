72b0442df17d8a1c6b71f12daa2d52fd1e454ab2
manishavi <manishalal972@gmail.com>
2018-05-07 15:25:21 -0500

    added action creator conatining request to backend api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '8139747d537ab0edf996b42c59cf1488';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
